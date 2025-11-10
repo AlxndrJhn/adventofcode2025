19d3259f4e5eda98a9d8b89f8c018ee65403e200
Alex <jahn.alexander@gmail.com>
2025-11-10 16:18:31 +0100

    feat: 2015 day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -29,10 +29,36 @@ pub fn part1(input: &str) -> usize {
     lights.iter().flat_map(|r| r.iter()).filter(|&&l| l).count()
 }
 
-// #[aoc(day6, part2)]
-// pub fn part2(input: &str) -> usize {
-//     input.lines().filter(|l| is_really_nice(l.trim())).count()
-// }
+#[aoc(day6, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut lights = [[0u32; 1000]; 1000];
+    input.lines().for_each(|line| {
+        let (action, rest) = if line.starts_with("turn on ") {
+            ("turn on", &line[8..])
+        } else if line.starts_with("turn off ") {
+            ("turn off", &line[9..])
+        } else if line.starts_with("toggle ") {
+            ("toggle", &line[7..])
+        } else {
+            panic!("invalid input");
+        };
+        let parts: Vec<&str> = rest.split(" through ").collect();
+        let start: Vec<usize> = parts[0].split(',').map(|s| s.parse().unwrap()).collect();
+        let end: Vec<usize> = parts[1].split(',').map(|s| s.parse().unwrap()).collect();
+
+        for x in start[0]..=end[0] {
+            for y in start[1]..=end[1] {
+                match action {
+                    "turn on" => lights[x][y] += 1,
+                    "turn off" => lights[x][y] = lights[x][y].saturating_sub(1),
+                    "toggle" => lights[x][y] += 2,
+                    _ => panic!("invalid action"),
+                }
+            }
+        }
+    });
+    lights.iter().flat_map(|r| r.iter()).sum::<u32>() as usize
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,5 +78,9 @@ mod tests {
             part1("turn on 0,0 through 999,999\nturn off 499,499 through 500,500"),
             1000000 - 4
         );
+    }
+    #[test]
+    fn example_4() {
+        assert_eq!(part2("turn on 0,0 through 0,0"), 1);
     }
 }
