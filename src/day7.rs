d68f624545c32b9abd96c1d76b7611f5908a5cbe
Alex <jahn.alexander@gmail.com>
2025-11-10 21:50:43 +0100

    feat: 2015 day 7 part 1

---
+++ src/day7.rs
@@ -0,0 +1,146 @@
+use std::collections::HashMap;
+
+pub fn resolve_wire_signal(input: &str) -> HashMap<&str, usize> {
+    let input_trimmed = input.trim();
+    let mut signals: HashMap<&str, usize> = HashMap::new();
+    let instructions: Vec<(&str, Vec<&str>)> = input_trimmed
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split(" -> ").collect();
+            let instr_parts: Vec<&str> = parts[0].split_whitespace().collect();
+            (parts[1], instr_parts)
+        })
+        .collect();
+
+    while signals.len() < instructions.len() {
+        for (wire, instr_parts) in &instructions {
+            if signals.contains_key(wire) {
+                continue;
+            }
+            let signal = match instr_parts.as_slice() {
+                [value] => {
+                    if let Ok(num) = value.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(value) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    }
+                }
+                ["NOT", operand] => {
+                    let val = if let Ok(num) = operand.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(operand) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    !val & 0xFFFF
+                }
+                [left, "AND", right] => {
+                    let left_val = if let Ok(num) = left.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(left) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    let right_val = if let Ok(num) = right.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(right) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    left_val & right_val
+                }
+                [left, "OR", right] => {
+                    let left_val = if let Ok(num) = left.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(left) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    let right_val = if let Ok(num) = right.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(right) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    left_val | right_val
+                }
+                [operand, "LSHIFT", shift] => {
+                    let val = if let Ok(num) = operand.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(operand) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    let shift_val: usize = shift.parse().unwrap();
+                    val << shift_val
+                }
+                [operand, "RSHIFT", shift] => {
+                    let val = if let Ok(num) = operand.parse::<usize>() {
+                        num
+                    } else {
+                        match signals.get(operand) {
+                            Some(&sig) => sig,
+                            None => continue,
+                        }
+                    };
+                    let shift_val: usize = shift.parse().unwrap();
+                    val >> shift_val
+                }
+                _ => panic!("invalid instruction"),
+            };
+            signals.insert(wire, signal);
+        }
+    }
+    return signals;
+}
+
+#[aoc(day7, part1)]
+pub fn part1(input: &str) -> usize {
+    let signals = resolve_wire_signal(input);
+    *signals.get("a").unwrap()
+}
+
+// #[aoc(day7, part2)]
+// pub fn part2(input: &str) -> usize {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let input = "
+123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i";
+        let result = resolve_wire_signal(input);
+        assert_eq!(result["d"], 72);
+        assert_eq!(result["e"], 507);
+        assert_eq!(result["f"], 492);
+        assert_eq!(result["g"], 114);
+        assert_eq!(result["h"], 65412);
+        assert_eq!(result["i"], 65079);
+        assert_eq!(result["x"], 123);
+        assert_eq!(result["y"], 456);
+    }
+}
