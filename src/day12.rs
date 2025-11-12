228ab632ed9459a4b71d84014da221d985362936
Alex <jahn.alexander@gmail.com>
2025-11-12 10:58:50 +0100

    2015d12p2

--- src/day12.rs
+++ src/day12.rs
@@ -9,9 +9,25 @@ pub fn part1(input: &str) -> isize {
         .sum()
 }
 
-// #[aoc(day12, part2)]
-// pub fn part2(input: &str) -> String {
-// }
+#[aoc(day12, part2)]
+pub fn part2(input: &str) -> i128 {
+    let json_value: serde_json::Value = serde_json::from_str(input).unwrap();
+    fn sum_json(value: &serde_json::Value) -> i128 {
+        match value {
+            serde_json::Value::Number(n) => n.as_i128().unwrap_or(0),
+            serde_json::Value::Array(arr) => arr.iter().map(sum_json).sum(),
+            serde_json::Value::Object(map) => {
+                if map.values().any(|v| v == "red") {
+                    0
+                } else {
+                    map.values().map(sum_json).sum()
+                }
+            }
+            _ => 0,
+        }
+    }
+    sum_json(&json_value)
+}
 
 #[cfg(test)]
 mod tests {
@@ -48,5 +64,21 @@ mod tests {
     #[test]
     fn example_8() {
         assert_eq!(part1("{}"), 0);
+    }
+    #[test]
+    fn example_9() {
+        assert_eq!(part2("[1,2,3]"), 6);
+    }
+    #[test]
+    fn example_10() {
+        assert_eq!(part2(r#"[1,{"c":"red","b":2},3]"#), 4);
+    }
+    #[test]
+    fn example_11() {
+        assert_eq!(part2(r#"{"d":"red","e":[1,2,3,4],"f":5}"#), 0);
+    }
+    #[test]
+    fn example_12() {
+        assert_eq!(part2(r#"[1,"red",5]"#), 6);
     }
 }
