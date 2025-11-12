8f8e0de68b655898e5307a51cea75fd0d1aef268
Alex <jahn.alexander@gmail.com>
2025-11-12 10:42:06 +0100

    template

---
+++ src/day.rs
@@ -0,0 +1,18 @@
+#[aoc(day__, part1)]
+pub fn part1(input: &str) -> usize {
+    0
+}
+
+// #[aoc(day__, part2)]
+// pub fn part2(input: &str) -> String {
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(part1("foo"), 0);
+    }
+}
---
+++ template.sh
@@ -0,0 +1,12 @@
+DAY_NUM=$1
+# if nothing provided, use today's date
+if [ -z "$DAY_NUM" ]; then
+    DAY_NUM=$(date +%-d)
+    echo "No day number provided, using today's date: $DAY_NUM"
+fi
+DAY_FILE="src/day${DAY_NUM}.rs"
+LIB_FILE="src/lib.rs"
+TEMPLATE_FILE="src/day.rs"
+cp $TEMPLATE_FILE $DAY_FILE
+sed -i "s/day__/day${DAY_NUM}/g" $DAY_FILE
+grep -q "pub mod day${DAY_NUM};" $LIB_FILE || sed -i "$(( $(wc -l < $LIB_FILE) - 3 ))i pub mod day${DAY_NUM};" $LIB_FILE
