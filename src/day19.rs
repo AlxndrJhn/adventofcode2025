892f3a9097232f3a109b037f0fdffe1c8f0e3fe0
Alex <jahn.alexander@gmail.com>
2025-11-13 12:56:54 +0100

    2015d19p2

--- src/day19.rs
+++ src/day19.rs
@@ -28,9 +28,46 @@ pub fn part1(input: &str) -> usize {
     distinct_molecules.len()
 }
 
-// #[aoc(day19, part2)]
-// pub fn part2(input: &str) -> usize {
-// }
+#[aoc(day19, part2)]
+pub fn part2(input: &str) -> usize {
+    let goal_molecule = input.lines().last().unwrap().trim();
+    let mut replacements = vec![];
+    for line in input.trim().lines() {
+        let line = line.trim();
+        if line.is_empty() || line == goal_molecule {
+            continue;
+        }
+        let parts: Vec<&str> = line.split(" => ").collect();
+        replacements.push((parts[0], parts[1]));
+    }
+    let mut heap = std::collections::BinaryHeap::new();
+    heap.push((0usize, goal_molecule.to_string()));
+    let mut visited = std::collections::HashSet::new();
+    while let Some((steps, molecule)) = heap.pop() {
+        if molecule == "e" {
+            return steps;
+        }
+        if visited.contains(&molecule) {
+            continue;
+        }
+        visited.insert(molecule.clone());
+        for (from, to) in &replacements {
+            let mut start_index = 0;
+            while let Some(index) = molecule[start_index..].find(to) {
+                let index = start_index + index;
+                let new_molecule = format!(
+                    "{}{}{}",
+                    &molecule[..index],
+                    from,
+                    &molecule[index + to.len()..]
+                );
+                heap.push((steps + 1, new_molecule));
+                start_index = index + 1;
+            }
+        }
+    }
+    panic!("No solution found");
+}
 
 #[cfg(test)]
 mod tests {
@@ -61,6 +98,38 @@ mod tests {
                     HOHOHO"
             ),
             7
+        );
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(
+            part2(
+                "   e => H
+                    e => O
+                    H => HO
+                    H => OH
+                    O => HH
+                    
+                    HOH"
+            ),
+            3
+        );
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(
+            part2(
+                "   e => H
+                    e => O
+                    H => HO
+                    H => OH
+                    O => HH
+                    
+                    HOHOHO"
+            ),
+            6
         );
     }
 }
