b87b273bfa24e1b19955ab2ac526fd664bfdd387
Alex <jahn.alexander@gmail.com>
2025-11-16 20:59:28 +0100

    16d6p2

--- src/day6.rs
+++ src/day6.rs
@@ -16,9 +16,23 @@ pub fn part1(input: &str) -> String {
     message
 }
 
-// #[aoc(day6, part2)]
-// pub fn part2(input: &str) -> usize {
-// }
+#[aoc(day6, part2)]
+pub fn part2(input: &str) -> String {
+    let input = input.trim();
+    let message_length = input.lines().next().unwrap().len();
+    // get most common char in each column
+    let mut message = String::new();
+    for col in 0..message_length {
+        let mut counts = std::collections::HashMap::new();
+        for line in input.lines() {
+            let c = line.trim().chars().nth(col).unwrap();
+            *counts.entry(c).or_insert(0) += 1;
+        }
+        let (&least_common_char, _) = counts.iter().min_by_key(|&(_, count)| count).unwrap();
+        message.push(least_common_char);
+    }
+    message
+}
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +61,32 @@ mod tests {
 "
             ),
             "easter"
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            part2(
+                "eedadn
+                drvtee
+                eandsr
+                raavrd
+                atevrs
+                tsrnev
+                sdttsa
+                rasrtv
+                nssdts
+                ntnada
+                svetve
+                tesnvt
+                vntsnd
+                vrdear
+                dvrsen
+                enarar
+"
+            ),
+            "advent"
         );
     }
 }
