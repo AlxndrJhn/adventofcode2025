552d556d40594ef1be47801556e7a81e59201f62
Alex <jahn.alexander@gmail.com>
2025-11-16 15:43:02 +0100

    2016d3p2

--- src/day3.rs
+++ src/day3.rs
@@ -11,9 +11,27 @@ pub fn part1(input: &str) -> usize {
     possible_triangles.count()
 }
 
-// #[aoc(day3, part2)]
-// pub fn part2(input: &str) -> usize {
-// }
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut possible_triangles = 0;
+    let lines: Vec<&str> = input.lines().collect();
+    for chunk in lines.chunks(3) {
+        let mut sides: Vec<Vec<usize>> = vec![vec![], vec![], vec![]];
+        for line in chunk {
+            for (i, num) in line.split_whitespace().enumerate() {
+                sides[i].push(num.parse().unwrap());
+            }
+        }
+        for side in sides {
+            let mut sorted_side = side.clone();
+            sorted_side.sort_unstable();
+            if sorted_side[0] + sorted_side[1] > sorted_side[2] {
+                possible_triangles += 1;
+            }
+        }
+    }
+    possible_triangles
+}
 
 #[cfg(test)]
 mod tests {
