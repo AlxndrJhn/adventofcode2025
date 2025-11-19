5782d7fb597ad5e182193414b009595cf2766204
Alex <jahn.alexander@gmail.com>
2025-11-19 16:34:08 +0100

    y16d15p2

--- src/day15.rs
+++ src/day15.rs
@@ -27,9 +27,33 @@ pub fn part1(input: &str) -> usize {
     unreachable!()
 }
 
-// #[aoc(day15, part2)]
-// pub fn part2(input: &str) -> usize {
-// }
+#[aoc(day15, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut disc_states = vec![];
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let positions: usize = parts[3].parse().unwrap();
+        let start_position: usize = parts[11].trim_end_matches('.').parse().unwrap();
+        disc_states.push(Disc {
+            positions,
+            start_position,
+        });
+    }
+    disc_states.push(Disc {
+        positions: 11,
+        start_position: 0,
+    });
+    for time in 0.. {
+        if disc_states
+            .iter()
+            .enumerate()
+            .all(|(i, disc)| (disc.start_position + time + i + 1) % disc.positions == 0)
+        {
+            return time;
+        }
+    }
+    unreachable!()
+}
 
 #[cfg(test)]
 mod tests {
