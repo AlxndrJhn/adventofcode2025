67d8b68e5bb60a42cc0fb4cee04299f92c1088cd
Alex <jahn.alexander@gmail.com>
2025-11-20 18:30:40 +0100

    y16d19p2

--- src/day19.rs
+++ src/day19.rs
@@ -21,9 +21,19 @@ pub fn part1(input: &str) -> usize {
     }
 }
 
-// #[aoc(day19, part2)]
-// pub fn part2(input: &str) -> usize {
-// }
+#[aoc(day19, part2)]
+pub fn part2(input: &str) -> usize {
+    //  source: https://www.reddit.com/r/adventofcode/comments/5j4lp1/comment/dbdgnwd/
+    let n: usize = input.trim().parse().unwrap();
+    let mut w = 1;
+    for i in 1..n {
+        w = w % i + 1;
+        if w > (i + 1) / 2 {
+            w += 1;
+        }
+    }
+    w
+}
 
 #[cfg(test)]
 mod tests {
@@ -32,5 +42,10 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(part1("5"), 3);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(part2("5"), 2);
     }
 }
