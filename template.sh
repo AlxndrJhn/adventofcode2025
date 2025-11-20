a5a91648ffb36ecb3c23b8546e696ecb6746dad4
Alex <jahn.alexander@gmail.com>
2025-11-20 17:34:56 +0100

    template auto disable other modules

--- template.sh
+++ template.sh
@@ -9,7 +9,9 @@ LIB_FILE="src/lib.rs"
 TEMPLATE_FILE="src/day.rs"
 cp $TEMPLATE_FILE $DAY_FILE
 sed -i "s/day__/day${DAY_NUM}/g" $DAY_FILE
+sed -i 's/^pub mod/\/\/ pub mod/' $LIB_FILE
 grep -q "pub mod day${DAY_NUM};" $LIB_FILE || sed -i "$(( $(wc -l < $LIB_FILE) - 3 ))i pub mod day${DAY_NUM};" $LIB_FILE
+# add "//" in front of all lines that start with "pub mod"
 cargo fmt
 echo "https://adventofcode.com/2016/day/${DAY_NUM}"
 code $DAY_FILE
