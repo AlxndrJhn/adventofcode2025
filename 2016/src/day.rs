11acc3f8751eecddfa523fe5f24535301decdf86
Alex <jahn.alexander@gmail.com>
2025-11-25 12:25:42 +0100

    chore: end of 2016

---
+++ 2016/src/day.rs
@@ -0,0 +1,46 @@
+#[aoc(day__, part1)]
+pub fn part1(input: &str) -> usize {
+    0
+}
+
+// #[aoc(day__, part2)]
+// pub fn part2(input: &str) -> usize {
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_2() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_3() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_4() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_5() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_6() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_7() {
+        assert_eq!(part1("foo"), 0);
+    }
+    #[test]
+    fn example_8() {
+        assert_eq!(part1("foo"), 0);
+    }
+}
--- src/day.rs
+++ src/day.rs
@@ -3,44 +3,44 @@ pub fn part1(input: &str) -> usize {
     0
 }
 
-// #[aoc(day__, part2)]
-// pub fn part2(input: &str) -> usize {
-// }
+#[aoc(day__, part2)]
+pub fn part2(input: &str) -> usize {
+    0
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn example_1() {
         assert_eq!(part1("foo"), 0);
     }
-    #[test]
-    fn example_2() {
-        assert_eq!(part1("foo"), 0);
-    }
-    #[test]
-    fn example_3() {
-        assert_eq!(part1("foo"), 0);
-    }
-    #[test]
-    fn example_4() {
-        assert_eq!(part1("foo"), 0);
-    }
-    #[test]
-    fn example_5() {
-        assert_eq!(part1("foo"), 0);
-    }
-    #[test]
-    fn example_6() {
-        assert_eq!(part1("foo"), 0);
-    }
-    #[test]
-    fn example_7() {
-        assert_eq!(part1("foo"), 0);
-    }
-    #[test]
-    fn example_8() {
-        assert_eq!(part1("foo"), 0);
-    }
+    // #[test]
+    // fn example_2() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
+    // #[test]
+    // fn example_3() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
+    // #[test]
+    // fn example_4() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
+    // #[test]
+    // fn example_5() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
+    // #[test]
+    // fn example_6() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
+    // #[test]
+    // fn example_7() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
+    // #[test]
+    // fn example_8() {
+    //     assert_eq!(part1("foo"), 0);
+    // }
 }
