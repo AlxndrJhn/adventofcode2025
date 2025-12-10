467083808334f41053acd277268aa2e884349998
Alex <jahn.alexander@gmail.com>
2025-12-10 11:51:51 +0100

    y25d10p2

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 ```shell
 for d in $(seq 1 25); do cargo aoc input -d $d -y 2016; done
 ```
+
+## 2025 day 10 part 2
+
+Requires the linear programming solver `sudo apt-get install coinor-cbc coinor-libcbc-dev`
--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,8 @@
+use core::panic;
+use good_lp::{
+    default_solver, variable, Expression, ProblemVariables, Solution, SolverModel, Variable,
+};
 use std::collections::{HashSet, VecDeque};
-
 #[aoc(day10, part1)]
 pub fn part1(input: &str) -> usize {
     let regex_pattern = r"^\[([.#]+)\] ((?:\(\d+(?:,\d+)*\) )+)\{((?:\d+,?)+)\}$";
@@ -58,7 +61,79 @@ fn bfs(is_on_lamps: Vec<bool>, buttons: &Vec<Vec<usize>>) -> usize {
 
 #[aoc(day10, part2)]
 pub fn part2(input: &str) -> usize {
-    0
+    let regex_pattern = r"^\[([.#]+)\] ((?:\(\d+(?:,\d+)*\) )+)\{((?:\d+,?)+)\}$";
+    let re = regex::Regex::new(regex_pattern).unwrap();
+    let mut button_presses = 0;
+    for line in input.lines() {
+        if let Some(captures) = re.captures(line) {
+            let buttons: Vec<Vec<usize>> = captures[2]
+                .trim()
+                .split(' ')
+                .map(|group| {
+                    group
+                        .trim_matches(|c| c == '(' || c == ')')
+                        .split(',')
+                        .filter_map(|num| num.parse::<usize>().ok())
+                        .collect()
+                })
+                .collect();
+
+            let joltage_ratings: Vec<usize> = captures[3]
+                .trim()
+                .split(',')
+                .filter_map(|num| num.parse::<usize>().ok())
+                .collect();
+
+            button_presses += lp(&buttons, &joltage_ratings);
+        }
+    }
+    button_presses
+}
+
+fn lp(buttons: &Vec<Vec<usize>>, target_state: &Vec<usize>) -> usize {
+    let d = target_state.len();
+    let n = buttons.len();
+
+    // map buttons to input vectors of dimension d
+    let mut inputs: Vec<Vec<f64>> = vec![vec![0.0; d]; n];
+    for (j, button) in buttons.iter().enumerate() {
+        for &lamp_index in button {
+            inputs[j][lamp_index] = 1.0;
+        }
+    }
+
+    let mut vars = ProblemVariables::new();
+    let x: Vec<Variable> = (0..n)
+        .map(|_| vars.add(variable().integer().min(0)))
+        .collect();
+
+    let mut objective = Expression::from(0.0);
+    for xi in &x {
+        objective += *xi;
+    }
+
+    let mut problem = vars.minimise(objective).using(default_solver);
+
+    // Constraints: for each dimension k: sum_j inputs[j][k] * x_j = target[k]
+    for k in 0..d {
+        let mut expr = Expression::from(0.0);
+        for j in 0..n {
+            let coeff = inputs[j][k];
+            if coeff != 0.0 {
+                expr += coeff * x[j];
+            }
+        }
+        problem = problem.with(expr.eq(target_state[k] as f64));
+    }
+
+    let solution = problem.solve();
+    if solution.is_err() {
+        panic!("No solution found");
+    }
+    let solution = solution.unwrap();
+
+    let result: Vec<f64> = x.iter().map(|xi| solution.value(*xi)).collect();
+    result.iter().sum::<f64>() as usize
 }
 
 #[cfg(test)]
@@ -93,20 +168,15 @@ mod tests {
             2
         );
     }
-    // #[test]
-    // fn example_5() {
-    //     assert_eq!(part1("foo"), 0);
-    // }
-    // #[test]
-    // fn example_6() {
-    //     assert_eq!(part1("foo"), 0);
-    // }
-    // #[test]
-    // fn example_7() {
-    //     assert_eq!(part1("foo"), 0);
-    // }
-    // #[test]
-    // fn example_8() {
-    //     assert_eq!(part1("foo"), 0);
-    // }
+    #[test]
+    fn example_5() {
+        assert_eq!(
+            part2(
+                "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
+            ),
+            33
+        );
+    }
 }
